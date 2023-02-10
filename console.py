b7fcb1598a3421be891527f9543d5598353e73c4
psych0wh1z <ikeagudike@gmail.com>
2023-02-10 18:39:12 +0100

    Change method behavior and add docs

--- console.py
+++ console.py
@@ -6,15 +6,22 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """ALX command line interpreter for AirBnB Clone"""
+
     intro = "Welcome to XBnB CLI. Enter 'help' for commands and 'quit' to exit"
     prompt = '(hbnb) '
 
+    def emptyline(self):
+        pass
+
     def do_quit(self, line):
-        """Quit command to exit the program"""
+        """Quit command to close the program"""
         return True
   
     def do_EOF(self, line):
         return True
+
+    def help_EOF(self):
+        print("EOF terminator to exit the interpreter")
 
     def postloop(self):
         print()

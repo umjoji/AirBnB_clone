f373a0f41b5ab53046e44947cb5812c65fe4b20d
psych0wh1z <ikeagudike@gmail.com>
2023-02-11 09:07:06 +0100

    Add create method to console

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """
 """
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -9,6 +10,18 @@ class HBNBCommand(cmd.Cmd):
 
     intro = "Welcome to XBnB CLI. Enter 'help' for commands and 'quit' to exit"
     prompt = '(hbnb) '
+
+    def do_create(self, line):
+        """Create new class instance and prints the id: create CLASS_NAME"""
+        class_name = line.strip()
+        if not class_name:
+            print("** class name missing **")
+        elif class_name not in BaseModel.__subclasses__() + ["BaseModel"]:
+            print("** class doesn't exist **")
+        else:
+            new_instance = eval(class_name)()
+            new_instance.save()
+            print(new_instance.id)
 
     def emptyline(self):
         pass

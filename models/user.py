67f30b589b7852d5c4a4e987460fb038be8edeff
psych0wh1z <ikeagudike@gmail.com>
2023-02-12 15:55:15 +0100

    Update command interpreter to work with User class

--- models/user.py
+++ models/user.py
@@ -2,8 +2,6 @@
 """user module contains a subclass of base_model.BaseModel()
 """
 from models.base_model import BaseModel
-import models
-import hashlib
 
 
 class User(BaseModel):
@@ -20,32 +18,3 @@ class User(BaseModel):
     last_name = ''
     password = ''
 
-    def __init__(self, *args, **kwargs):
-        classes = {"email": self.email, "first_name": '',
-                   "last_name": '', "password": ''}
-        super().__init__(**classes)
-        super().save()
-
-    def save(self):
-        """
-        """
-        self.password = hash_password(self.password) 
-        models.storage.new(self)
-        
-    def to_dict(self):
-        """
-        """
-        dictionary = super().__dict__.copy()
-        dictionary["email"] = User.email
-        return dictionary
-
-
-def hash_password(password):
-    # Create the md5 hash object
-    md5 = hashlib.md5()
-    
-    # Update the hash with the password
-    md5.update(password.encode())
-    
-    # Return the hexadecimal representation of the hash
-    return md5.hexdigest()

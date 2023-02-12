dbdc4696ea68c430294829f898e0d9aa84a35a85
psych0wh1z <ikeagudike@gmail.com>
2023-02-12 14:47:45 +0100

    Add more features

--- models/user.py
+++ models/user.py
@@ -2,6 +2,8 @@
 """user module contains a subclass of base_model.BaseModel()
 """
 from models.base_model import BaseModel
+import models
+import hashlib
 
 
 class User(BaseModel):
@@ -17,3 +19,33 @@ class User(BaseModel):
     first_name = ''
     last_name = ''
     password = ''
+
+    def __init__(self, *args, **kwargs):
+        classes = {"email": self.email, "first_name": '',
+                   "last_name": '', "password": ''}
+        super().__init__(**classes)
+        super().save()
+
+    def save(self):
+        """
+        """
+        self.password = hash_password(self.password) 
+        models.storage.new(self)
+        
+    def to_dict(self):
+        """
+        """
+        dictionary = super().__dict__.copy()
+        dictionary["email"] = User.email
+        return dictionary
+
+
+def hash_password(password):
+    # Create the md5 hash object
+    md5 = hashlib.md5()
+    
+    # Update the hash with the password
+    md5.update(password.encode())
+    
+    # Return the hexadecimal representation of the hash
+    return md5.hexdigest()

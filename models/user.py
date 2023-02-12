1e97f9fdc80d6f3e82f0eb294c71ad0ac9cc20ac
psych0wh1z <ikeagudike@gmail.com>
2023-02-12 12:08:14 +0100

    Add user class

---
+++ models/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""user module contains a subclass of base_model.BaseModel()
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Defines a class that inherits from BaseModel
+
+        Attributes:
+            email (str): user's email
+            first_name (str): user's first name
+            last_name (str): user's last name
+            password (str): user's password
+    """
+    email = ''
+    first_name = ''
+    last_name = ''
+    password = ''
